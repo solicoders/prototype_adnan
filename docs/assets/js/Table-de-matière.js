8160484d96e10d05aa7eb7fee98c9613d6bbc3ac
imrane sarsri <imransarsi.ii@gmail.com>
2024-02-21 15:06:07 +0100

    save

--- docs/assets/js/Table-de-matière.js
+++ docs/assets/js/Table-de-matière.js
@@ -1,22 +1,34 @@
-//  Table de matière
+// Table de matière
 const tableDeMatier = document.getElementById('table-de-matière');
-const h1Elements = document.querySelectorAll('h1');
+const h1AndH2Elements = document.querySelectorAll('h1, h2'); // Select both h1 and h2 elements
 
 // Create a new unordered list element
 const ul = document.createElement('ul');
 
-h1Elements.forEach(function (h1) {
+h1AndH2Elements.forEach(function (element) {
+
     // Create a new list item element
     const li = document.createElement('li');
+    const p = document.createElement('p');
 
-    // Create a new anchor element, set its href attribute to the corresponding h1's id, set its text content to the h1's text content, and insert the new anchor into the list item
+    // Create a new anchor element, set its href attribute to the corresponding element's id, set its text content to the element's text content, and insert the new anchor into the list item
     const newAnchor = document.createElement('a');
-    newAnchor.href = "#" + h1.id;
-    newAnchor.textContent = h1.textContent;
+    newAnchor.href = "#" + element.id;
+    newAnchor.textContent = element.textContent;
     newAnchor.style.fontWeight = "bold"; // Adding bold font style
-
     // Append the anchor element to the list item
-    li.appendChild(newAnchor);
+
+    if (element.tagName === 'H1') {
+        li.appendChild(newAnchor);
+
+    }
+
+    if (element.tagName === 'H2') {
+        li.appendChild(newAnchor);
+        ul.aulpendChild(li);
+        li.appendChild(ul);
+    }
+    // console.log(element)
 
     // Append the list item to the unordered list
     ul.appendChild(li);
@@ -24,3 +36,4 @@ h1Elements.forEach(function (h1) {
 
 // Insert the unordered list after the table-de-matier element
 tableDeMatier.insertAdjacentElement('afterend', ul);
+
